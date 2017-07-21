843d276b50f47f1bb6e1f2f5860a2536d8260d8a
thotmx <hermes.o.r@gmail.com>
2017-07-21 15:53:43 -0500

    Extract functions for kocify tasks in Ubermix

--- kocify.sh
+++ kocify.sh
@@ -4,39 +4,52 @@ shopt -s extglob
 set -o errtrace
 set -o errexit
 
+function ubermix_update_packages {
+  sudo apt-mark hold grub2-common grub-common grub-pc grub-pc-bin
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+}
+
+function ubermix_install_wireless_drivers {
+  # Add wireless drivers
+  sudo apt-get -y purge bcmwl-kernel-source
+  sudo apt-get -y install firmware-b43legacy-installer
+  sudo apt-get -y install firmware-b43-installer
+}
+
+function ubermix_install_software {
+  software=( $(curl -sSL https://raw.githubusercontent.com/kidsoncomputers/kocifier/master/ubermix_packages) )
+
+  for package in ${software[*]}
+  do
+    sudo apt-get -y install $package
+  done
+}
+
+function remove_keyboard_packages {
+  sudo apt-get remove fcitx*
+}
+
+function ubermix_kocify {
+  ubermix_update_packages
+  ubermix_install_wireless_drivers
+  ubermix_install_software
+  # Change the wallpaper using commands
+  # Install Kids Ruby
+  # Change regional formats
+  # Configure input methods
+  remove_keyboard_packages
+  # Change the timezone to Mexico City
+  # Install Language Pack support for Spanish
+}
+
 #######################
 # Ubermix customization
 #######################
 
-# Update packages
-sudo apt-mark hold grub2-common grub-common grub-pc grub-pc-bin
-sudo apt-get -y update
-sudo apt-get -y upgrade
+ubermix_kocify
 
-# Add wireless drivers
-sudo apt-get -y purge bcmwl-kernel-source
-sudo apt-get -y install firmware-b43legacy-installer
-sudo apt-get -y install firmware-b43-installer
 
-# Install software
 
-software=( $(curl -sSL https://raw.githubusercontent.com/kidsoncomputers/kocifier/master/ubermix_packages) )
 
-for package in ${software[*]}
-do
-  sudo apt-get -y install $package
-done
 
-# Change the wallpaper using commands
-
-# Install Kids Ruby
-
-# Change regional formats
-
-# Configure input methods
-
-sudo apt-get remove fcitx*
-
-# Change the timezone to Mexico City
-
-# Install Language Pack support for Spanish
