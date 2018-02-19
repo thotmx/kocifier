843183c47d9b3056766355c2223229afc3596d89
IrvingMg <IrvingMg@users.noreply.github.com>
2018-02-18 23:52:36 -0600

    kocify.sh
    Change wallpaper automatically

--- kocify.sh
+++ kocify.sh
@@ -55,11 +55,29 @@ function change_timezone {
   sudo ln -s /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 }
 
+function ubermix_download_background_image {
+	cd /tmp/
+	wget https://raw.githubusercontent.com/kidsoncomputers/kocifier/master/assets/Wallpaper%204x3.png
+	sudo cp 'Wallpaper 4x3.png' /usr/share/backgrounds
+	sudo chmod a+rw /usr/share/backgrounds/*
+}
+
+function ubermix_configuration_background_image { 	
+	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/Wallpaper 4x3.png'
+	gsettings set org.gnome.desktop.background picture-options 'scaled'
+  gsettings set org.gnome.desktop.background primary-color '#ffffff'
+}
+
+function ubermix_background_main {
+	ubermix_download_background_image
+	ubermix_configuration_background_image
+}
+
 function ubermix_kocify {
   ubermix_update_packages
   ubermix_install_wireless_drivers
   ubermix_install_software
-  # Change the wallpaper using commands
+  ubermix_background_main
   # Install Kids Ruby
   # Change regional formats
   # Configure input methods
